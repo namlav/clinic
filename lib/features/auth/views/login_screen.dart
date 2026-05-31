d050c99eaa591140807f499acc7edfef32653ff5
namlav <lavannam312@gmail.com>
2026-06-01 02:15:57 +0700

    feat(ui): add authentification feature views

--- lib/features/auth/views/login_screen.dart
+++ lib/features/auth/views/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'register_screen.dart';
 import 'forgot_password_screen.dart';
 
--- lib/features/auth/views/register_screen.dart
+++ lib/features/auth/views/register_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'otp_verification_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
